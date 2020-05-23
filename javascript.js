eff7d351b31dbe539d57bc011309aca0fbf68717
adilahmad321 <adilahmad95@gmail.com>
2020-05-23 10:37:49 +0500

    Implemented Work and Rest Timers

--- javascript.js
+++ javascript.js
@@ -1,15 +1,28 @@
-let minutes = 25;
+let workInterval = 2;
+let restInterval = 1;
+let minutes = 0;
 let seconds = 0;
 let timer;
 let initialized = false;
+let stateWork = true;
 
 
 function countdown() {
-    if (seconds < 10){
-        console.log(minutes + ":0" + seconds);
+    if (minutes < 10){
+        if (seconds < 10){
+            console.log("0" + minutes + ":0" + seconds);
+        }
+        else{
+            console.log("0" + minutes + ":" + seconds);
+        }
     }
     else{
-        console.log(minutes + ":" + seconds);
+        if (seconds < 10){
+        console.log(minutes + ":0" + seconds);
+        }
+        else{
+            console.log(minutes + ":" + seconds);
+        }
     }
     if(seconds == 0){
         minutes--;
@@ -17,6 +30,16 @@ function countdown() {
     }
     else{
         seconds--;
+    }
+    if (minutes === 0 && seconds === 0){
+        if (stateWork){
+            rest();
+            stateWork = false;
+        }
+        else{
+            work();
+            stateWork = true;
+        }
     }
 }
 
@@ -39,4 +62,22 @@ function startTimer(mins, secs) {
     minutes = mins;
     seconds = secs;
     timer = setInterval(countdown, 1000);
+}
+
+function work() {
+    stateWork = true;
+    startTimer(workInterval, 0);
+}
+
+function rest() {
+    stateWork = false;
+    startTimer(restInterval, 0);
+}
+
+function changeWork(n) {
+    workInterval = n;
+}
+
+function changeRest(n) {
+    restInterval = n;
 }
