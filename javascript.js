a91d3cad3f53beec24791e6867296fbd31fbdc7c
adilahmad321 <adilahmad95@gmail.com>
2020-05-25 10:12:48 +0500

    Implemented time logic via buttons

--- javascript.js
+++ javascript.js
@@ -1,13 +1,39 @@
-let workInterval = 2;
-let restInterval = 1;
+let workInterval = 25;
+let restInterval = 5;
 let minutes = 0;
 let seconds = 0;
 let timer;
 let initialized = false;
 let stateWork = true;
 
+const countdownInterval = 1000; // in milliseconds
+const timerText = document.getElementById("timer-text");
+const currInterval = document.getElementById("top-controls");
 
 function countdown() {
+    updateTimerText();
+    if(seconds == 0){
+        minutes--;
+        seconds = 59;
+    }
+    else{
+        seconds--;
+    }
+    if (minutes === 0 && seconds === 0){
+        if (stateWork){
+            rest();
+            currInterval.textContent = "Interval: Break";
+            stateWork = false;
+        }
+        else{
+            work();
+            currInterval.textContent = "Interval: Session";
+            stateWork = true;
+        }
+    }
+}
+
+function updateTimerText() {
     const hours = Math.floor(minutes/60);
     const mins = minutes%60;
     let timeString = "";
@@ -22,49 +48,53 @@ function countdown() {
         timeString += "0";
     }
     timeString += seconds;
-    console.log(timeString);
-    if(seconds == 0){
-        minutes--;
-        seconds = 59;
-    }
-    else{
-        seconds--;
-    }
-    if (minutes === 0 && seconds === 0){
-        if (stateWork){
-            rest();
-            stateWork = false;
-        }
-        else{
-            work();
-            stateWork = true;
-        }
-    }
+    timerText.textContent = timeString;
 }
 
-function stopTimer() {
+function pauseTimer() {
     clearInterval(timer);
+    play.disabled = false;
+    pause.disabled = true;
 }
 
 function resumeTimer() {
-    timer = setInterval(countdown, 1000);
+    timer = setInterval(countdown, countdownInterval);
 }
 
 function resetTimer() {
-    stopTimer();
+    currInterval.textContent = "Interval: Session";
+    pauseTimer();
+    changeWork(25);
+    changeRest(5);
+    initialized = false;
+}
+
+function stopTimer() {
+    currInterval.textContent = "Interval: Session";
+    pauseTimer();
     seconds = 0;
-    minutes = 25;
+    minutes = workInterval;
+    initialized = false;
+    updateTimerText();
+    sessionUp.disabled = false;
+    sessionDown.disabled = false;
+    breakUp.disabled = false;
+    breakDown.disabled = false;
+    breakText.disabled = false;
+    sessionText.disabled = false;
+    stop.disabled = true;
+    reset.disabled = false;
 }
 
 function startTimer(mins, secs) {
     if(initialized){
-        stopTimer();
+        clearInterval(timer);
     } else {
         initialized = true;
     }
     minutes = mins;
     seconds = secs;
-    timer = setInterval(countdown, 1000);
+    timer = setInterval(countdown, countdownInterval);
 }
 
 function work() {
@@ -78,9 +108,88 @@ function rest() {
 }
 
 function changeWork(n) {
-    workInterval = n;
+    if ((n > 0) && (n <= 300)){
+        workInterval = n;
+        minutes = workInterval;
+        updateTimerText();
+    }
+    sessionText.value = workInterval;
+    console.log(workInterval);
 }
 
 function changeRest(n) {
-    restInterval = n;
+    if ((n > 0) && (n <= 300)){
+        restInterval = n;
+    }
+    breakText.value = restInterval;
+    console.log(restInterval);
 }
+
+const sessionText = document.getElementById("sessionText");
+const breakText = document.getElementById("breakText");
+
+sessionText.addEventListener("change", (e) => {
+    changeWork(Number(sessionText.value));
+}); 
+
+breakText.addEventListener("change", (e) => {
+    changeRest(Number(breakText.value));
+});
+
+function incrementSession() {
+    changeWork(workInterval+1);
+}
+
+function decrementSession() {
+    changeWork(workInterval-1);
+}
+
+function incrementBreak() {
+    changeRest(restInterval+1);
+}
+
+function decrementBreak() {
+    changeRest(restInterval-1);
+}
+
+function playTimer() {
+    if (initialized){
+        resumeTimer();
+    } else {
+        startTimer(workInterval, 0);
+    }
+    play.disabled = true;
+    pause.disabled = false;
+    stop.disabled = false;
+    reset.disabled = true;
+    sessionUp.disabled = true;
+    sessionDown.disabled = true;
+    breakUp.disabled = true;
+    breakDown.disabled = true;
+    breakText.disabled = true;
+    sessionText.disabled = true;
+}
+
+const sessionUp = document.getElementById("sessionUp");
+const sessionDown = document.getElementById("sessionDown");
+const breakUp = document.getElementById("breakUp");
+const breakDown = document.getElementById("breakDown");
+
+sessionUp.addEventListener("click", incrementSession);
+sessionDown.addEventListener("click", decrementSession);
+breakUp.addEventListener("click", incrementBreak);
+breakDown.addEventListener("click", decrementBreak);
+
+const play = document.getElementById("play");
+const pause = document.getElementById("pause");
+const stop = document.getElementById("stop");
+const reset = document.getElementById("reset");
+
+play.addEventListener("click", playTimer);
+pause.addEventListener("click", pauseTimer);
+stop.addEventListener("click", stopTimer);
+reset.addEventListener("click", resetTimer);
+
+pause.disabled = true;
+stop.disabled = true;
+reset.disabled = true;
